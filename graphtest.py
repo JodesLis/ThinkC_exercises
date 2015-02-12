7b835ca71591eaeee5e1600b8fcb4b279fc82b93
JodesLis <jodesapplespice@gmail.com>
2015-02-12 15:46:43 +0000

    Need to do add_regular_edges

---
+++ graphtest.py
@@ -0,0 +1,29 @@
+
+from graph import Vertex
+from graph import Edge
+from graph import Graph
+import GraphWorld
+
+
+
+
+
+
+
+# def main(script, n='10', *args):
+#
+#     # create n Vertices
+#     n = int(n)
+#     labels = string.ascii_lowercase + string.ascii_uppercase
+#     vs = [Vertex(c) for c in labels[:n]]
+#
+#     # create a graph and a layout
+#     g = Graph(vs)
+#     g.add_all_edges()
+#     layout = CircleLayout(g)
+#
+#     # draw the graph
+#     gw = GraphWorld()
+#     gw.show_graph(g, layout)
+#     gw.mainloop()
+
